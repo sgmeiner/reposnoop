bbf9928e06d2f439857fcf9d91f693e7f2dd1e87
sgmeiner <51077662+sgmeiner@users.noreply.github.com>
2022-06-07 23:42:36 +0200

    working on request formula

--- ghclient.py
+++ ghclient.py
@@ -3,26 +3,58 @@
 2022 by Stefan Groh, stefan_groh@gmx.de
 """
 import requests
+import urllib.parse as urls
 
 
-API_SEARCH = "https://api.github.com/search/repositories?"
+API_SEARCH = "https://api.github.com/search/repositories?q="
 API_COMMITS = "https://api.github.com/repos/OWNER/REPO/stats/commit_activity"
 API_CONTRIBUTORS = "https://api.github.com/repos/OWNER/REPO/stats/contributors"
+
+endpoints = {
+            "API_SEARCH": "https://api.github.com/search/repositories?q=",
+            "API_COMMITS": "https://api.github.com/repos/OWNER/REPO/"
+                           "stats/commit_activity?",
+            "API_CONTRIBUTORS": "https://api.github.com/repos/OWNER/REPO/"
+                                "stats/contributors?"
+            }
+
+
+def prepare_url(endpoint, payload):
+    """Prepare URL at <endpoint> with <payload> for request."""
+    if endpoint in endpoints:
+        complete_endpoint = endpoint.replace(
+                        "OWNER",
+                        urls.quote(payload["owner"])).replace(
+                        "REPO",
+                        urls.quote(payload["repo"]))
+        search_string = urls.quote_plus("+".join(payload["search_kw"]))
+        qualifiers = urls.quote()
+
+        # ---------------- !!! ---------------------
+    else:
+        print("Request for unknown endpoint: ", endpoint)
+        raise ValueError
+        return None
+
+    return new_url
 
 
 def get_repo_search(keywords="", qualifiers=""):
     """Run search query request and return selected item data as list."""
-    search_kw = keywords if keywords else ["intelligence", "twitter", "language%3Apython"]
-    search_qual = qualifiers if qualifiers else ["ref=advsearch", "per_page=100"]
+    search_kw = keywords if keywords else ["intelligence",
+                                           "twitter",
+                                           "language%3Apython"]
+    search_qual = qualifiers if qualifiers else ["ref=advsearch",
+                                                 "per_page=100"]
 
     # preparing URL
     search_url = "&".join([API_SEARCH,
-                           f"q={'+'.join(search_kw)}",
+                           f"{'+'.join(search_kw)}",
                            *search_qual])
 
     # sending out request. URL should still be built better
     search_response = requests.get(search_url)
-    
+
     # parse response with requests json parser
     result_json = search_response.json()
 
@@ -55,7 +87,7 @@ def github_client_main():
     repo_list = get_repo_search(keywords=["insurance", "data", "analysis"],
                                 qualifiers=["ref=advsearch", "per_page=5"])
     print("\n\n".join([str(item) for item in repo_list]))
-    
+
 
 if __name__ == "__main__":
     github_client_main()
